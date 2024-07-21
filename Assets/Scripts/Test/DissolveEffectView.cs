0bc3b8419fe832cab08445bfb54fbb17eda30bb0
storozhuk <storozhuk@quartsoft.com>
2024-07-21 19:46:11 +0300

    Add Universal RP. Add dissolve shader effect.

---
+++ Assets/Scripts/Test/DissolveEffectView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Test
+{
+    public class DissolveEffectView : MonoBehaviour
+    {
+        [SerializeField] private Material _material;
+
+        private float _dissolveValue;
+        private bool _isDissolving;
+
+        private void OnEnable()
+        {
+            _isDissolving = true;
+        }
+
+        private void Update()
+        {
+            if(_isDissolving)
+            {
+                _dissolveValue = Mathf.Clamp01(_dissolveValue + Time.deltaTime);
+                _material.SetFloat("_DissolveValue", _dissolveValue);
+            }
+        }
+        
+        // private IEnumerator CreateCoroutine(Action callback, float milliseconds)
+        // {
+        //     var timeSeconds = milliseconds / 1000;
+        //     
+        //     
+        //     
+        //     yield return new WaitForSeconds(timeSeconds);
+        //     callback?.Invoke();
+        // }
+    }
+}
